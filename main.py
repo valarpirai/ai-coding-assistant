a8cd425b0da7740b8708cc94314b4faa90d1f998
Valarpirai <a.valarpirai@gmail.com>
2025-04-23 16:47:07 +0530

    Colored output

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from dotenv import load_dotenv
 import os
 from anthropic import Anthropic
+from colorama import init, Fore, Style
+
 from tools import read_file, write_file, list_files
 
 load_dotenv()
+
+init()
 
 class Assistant:
     def __init__(self):
@@ -18,27 +22,28 @@ class Assistant:
     def add_assistant_message(self, message: str):
         self.messages.append({"role": "assistant", "content": message})
 
-    def make_request(self, message: str):
+    def run_inference(self, message: str):
         self.add_user_message(message)
         response = self.client.messages.create(
             model="claude-3-5-sonnet-20240620",
             max_tokens=100,
-            messages=self.messages
+            messages=self.messages,
         )
         self.add_assistant_message(response.content[0].text)
         return response.content[0].text
     
     def get_user_input(self):
-        return input("Chat with the assistant: ")
+        return input(Fore.BLUE + "You: " + Style.RESET_ALL)
 
     def run(self):
         try:
+            print("\nChat with Claude (use 'ctrl-c' to quit)\n")
             while True:
                 user_input = self.get_user_input()
-                response_content = self.make_request(user_input)
-                print("\n Assistant: ", response_content)
+                response_content = self.run_inference(user_input)
+                print(Fore.GREEN + "Claude:" + Style.RESET_ALL, response_content, "\n")
         except KeyboardInterrupt:
-            print("Assistant: Exiting...")
+            print(Fore.RED, "\nAssistant: Exiting...")
 
 def main():
     assistant = Assistant()
